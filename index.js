3d8a698ea6fe6ab76922986b169bccc0fac2676d
Kian F <kfrawley@binghamton.edu>
2024-08-09 14:25:00 -0400

    Added Projectiles

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ class Player {
             this.height=image.height * scale
             this.position = {
                 x:(canvas.width/2) - (this.width/2),
-                y:(canvas.height) - (this.height-10)
+                y:(canvas.height) - (this.height)
             }
         }
     }
@@ -49,9 +49,36 @@ class Player {
             this.position.x += this.velocity.x
         }
     }
+
+}
+
+class Projectile{
+    constructor({position,velocity}){
+        this.position = position
+        this.velocity = velocity
+
+        this.radius = 3
+    }
+
+    draw(){
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2)
+        c.fillStyle='red'
+        c.fill()
+        c.closePath()
+    }
+
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
 }
 
 const player = new Player ()
+const projectiles = [
+  
+]
 const keys = {
     a: {
         pressed: false
@@ -70,6 +97,15 @@ function animate(){
     c.fillStyle = 'black'
     c.fillRect(0,0,canvas.width,canvas.height)
     player.update()
+    projectiles.forEach((projectile, index) => {
+        if(projectile.position.y + projectile.radius <= 0) {
+            setTimeout(()=>{
+                projectiles.splice(index, 1)
+            },0)
+        }else{
+            projectile.update()
+        }
+    })
 
     if(keys.a.pressed && player.position.x>=0){
         player.velocity.x = -7
@@ -96,7 +132,19 @@ addEventListener('keydown', ({key})=> {
             break
         case ' ':
             console.log('space')
-            break   
+            projectiles.push(new Projectile({
+                position: {
+                    x: player.position.x+(player.width/2), 
+                    y: player.position.y
+                },
+                velocity: {
+                    x: 0,
+                    y: -10
+                }
+            })
+        )
+        //console.log(projectiles)
+        break   
     }
 })
 
