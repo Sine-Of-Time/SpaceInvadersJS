a132aac11ebd6f27cedcdbc2550c268f59c70a3f
Kian F <kfrawley@binghamton.edu>
2024-08-11 15:14:10 -0400

    Added grid spawning

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ class Grid {
 
 const player = new Player ()
 const projectiles = []
-const grids = [new Grid()]
+const grids = []
 
 const keys = {
     a: {
@@ -179,6 +179,10 @@ const keys = {
     }
 }
 
+let frames = 0
+let randomInterval = Math.floor((Math.random()*500)+500)
+
+console.log(randomInterval)
 
 function animate(){
     requestAnimationFrame(animate)
@@ -210,6 +214,17 @@ function animate(){
     }else{
         player.velocity.x = 0 
     }
+
+    console.log(frames)
+    //spawning invaders
+    if (frames%randomInterval==0){
+        grids.push(new Grid())
+        randomInterval = Math.floor((Math.random()*500)+500)
+        frames=0
+        console.log(randomInterval)
+    }
+
+    frames++
 }
 
 animate()
